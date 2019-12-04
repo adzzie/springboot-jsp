19673d0bdc979d51361cdd43d665e23d892e93c8
ghozal <ghozal@gmail.com>
2019-12-05 00:25:33 +0700

    initialize

---
+++ src/main/java/com/springboot/maven/jsp/JspApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.maven.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/maven/jsp/JspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.maven.jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
